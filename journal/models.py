8b36286241f6bb17f40f93f2ba64c95db559eade
trevthedev777 <bloodsportrecords@gmail.com>
2022-11-22 22:05:00 +0000

    create database models

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft', (1, 'Published')))
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='journal_entry')  # noqa
+    created_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()  
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='journal_likes', blank=True)  # noqa
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Reply(models.Model):
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='replies')  # noqa
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    liked = models.Boolean(False)
+    report = models.Boolean(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f'Comment {self.body} by {self.name}'
